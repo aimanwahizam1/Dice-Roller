16f6c2e1fa6713837c0f5aba6e1b792a4d46d7ce
aiman.wahizam <aiman.wahizam@nology.io>
2024-04-24 12:05:50 +0400

    added custom dice func, changed graph viz for custom dice and refactored

--- main.py
+++ main.py
@@ -12,107 +12,145 @@ import matplotlib.pyplot as plt
 
 # ----------------------------------- Setup ---------------------------------- #
 
-print("Welcome to...")
-time.sleep(1)
-print("""
- .----------------.  .----------------.  .----------------.  .----------------.   .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
-| .--------------. || .--------------. || .--------------. || .--------------. | | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
-| |  ________    | || |     _____    | || |     ______   | || |  _________   | | | |  _______     | || |     ____     | || |   _____      | || |   _____      | || |  _________   | || |  _______     | |
-| | |_   ___ `.  | || |    |_   _|   | || |   .' ___  |  | || | |_   ___  |  | | | | |_   __ \    | || |   .'    `.   | || |  |_   _|     | || |  |_   _|     | || | |_   ___  |  | || | |_   __ \    | |
-| |   | |   `. \ | || |      | |     | || |  / .'   \_|  | || |   | |_  \_|  | | | |   | |__) |   | || |  /  .--.  \  | || |    | |       | || |    | |       | || |   | |_  \_|  | || |   | |__) |   | |
-| |   | |    | | | || |      | |     | || |  | |         | || |   |  _|  _   | | | |   |  __ /    | || |  | |    | |  | || |    | |   _   | || |    | |   _   | || |   |  _|  _   | || |   |  __ /    | |
-| |  _| |___.' / | || |     _| |_    | || |  \ `.___.'\  | || |  _| |___/ |  | | | |  _| |  \ \_  | || |  \  `--'  /  | || |   _| |__/ |  | || |   _| |__/ |  | || |  _| |___/ |  | || |  _| |  \ \_  | |
-| | |________.'  | || |    |_____|   | || |   `._____.'  | || | |_________|  | | | | |____| |___| | || |   `.____.'   | || |  |________|  | || |  |________|  | || | |_________|  | || | |____| |___| | |
-| |              | || |              | || |              | || |              | | | |              | || |              | || |              | || |              | || |              | || |              | |
-| '--------------' || '--------------' || '--------------' || '--------------' | | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
- '----------------'  '----------------'  '----------------'  '----------------'   '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
-      """)
-time.sleep(0.9)
-print("In this version we will roll 2 d6")
-time.sleep(0.9)
-print("Let's get rolling!\n")
-time.sleep(0.9)
+def setup():
+    print("Welcome to...")
+    time.sleep(1)
+    print("""
+    .----------------.  .----------------.  .----------------.  .----------------.   .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+    | .--------------. || .--------------. || .--------------. || .--------------. | | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+    | |  ________    | || |     _____    | || |     ______   | || |  _________   | | | |  _______     | || |     ____     | || |   _____      | || |   _____      | || |  _________   | || |  _______     | |
+    | | |_   ___ `.  | || |    |_   _|   | || |   .' ___  |  | || | |_   ___  |  | | | | |_   __ \    | || |   .'    `.   | || |  |_   _|     | || |  |_   _|     | || | |_   ___  |  | || | |_   __ \    | |
+    | |   | |   `. \ | || |      | |     | || |  / .'   \_|  | || |   | |_  \_|  | | | |   | |__) |   | || |  /  .--.  \  | || |    | |       | || |    | |       | || |   | |_  \_|  | || |   | |__) |   | |
+    | |   | |    | | | || |      | |     | || |  | |         | || |   |  _|  _   | | | |   |  __ /    | || |  | |    | |  | || |    | |   _   | || |    | |   _   | || |   |  _|  _   | || |   |  __ /    | |
+    | |  _| |___.' / | || |     _| |_    | || |  \ `.___.'\  | || |  _| |___/ |  | | | |  _| |  \ \_  | || |  \  `--'  /  | || |   _| |__/ |  | || |   _| |__/ |  | || |  _| |___/ |  | || |  _| |  \ \_  | |
+    | | |________.'  | || |    |_____|   | || |   `._____.'  | || | |_________|  | | | | |____| |___| | || |   `.____.'   | || |  |________|  | || |  |________|  | || | |_________|  | || | |____| |___| | |
+    | |              | || |              | || |              | || |              | | | |              | || |              | || |              | || |              | || |              | || |              | |
+    | '--------------' || '--------------' || '--------------' || '--------------' | | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+    '----------------'  '----------------'  '----------------'  '----------------'   '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+        """)
 
-# ----------------------------- Global Variables ----------------------------- #
+    time.sleep(0.9)
 
-number_of_rolls = 1
-roll_history = {
-    2:0,
-    3:0,
-    4:0,
-    5:0,
-    6:0,
-    7:0,
-    8:0,
-    9:0,
-    10:0,
-    11:0,
-    12:0,
-}
+# ------------------------------- Roll History ------------------------------- #
+
+def create_roll_history(dice_array):
+    history = {}
+    
+    for i in range(len(dice_array), sum(dice_array) + 1):
+        history[i] = 0
+
+    return history
+
+# -------------------------------- Custom Dice ------------------------------- #
+
+def custom_dice_setup():
+    faces_array = []
+
+    while True:
+        try:
+            number_of_die = int(input("How many dice are you rolling?\n"))
+        except ValueError:
+            print("Input a valid number of dice.")
+            continue
+        else:
+            print(f'Rolling with {number_of_die} dice.')
+            break
+
+    for i in range(1, number_of_die + 1):
+        while True:
+            try:
+                die_face = int(input(f'How many faces are on dice {i}?\n'))
+            except ValueError:
+                print("Input a valid dice face value.")
+                continue
+            else:
+                print(f'Dice {i} is a d{die_face}.')
+                faces_array.append(die_face)
+                break
+    
+    return faces_array
 
 # ------------------------------ Graph Creation ------------------------------ #
 
-def create_graph(array, total_rolls):
-    plt.bar(array.keys(),array.values(), color = (1,0,0,0.5))
+def create_graph(rolls_array, total_rolls, dice_array):
+    plt.bar(rolls_array.keys(), rolls_array.values(), color = (1,0,0,0.5))
 
     plt.xlabel("Dice Roll")
-    plt.xticks(range(2,13))
+    plt.xticks(range(len(dice_array), sum(dice_array) + 1))
     plt.ylabel("Frequency")
 
     title = "Dice Roll Frequency. Total Rolls = " + str(total_rolls)
     plt.title(title)
 
-    if max(array.values()) > 10:
-        plt.yticks(range(0, max(array.values()) + 1, 2))
+    if max(rolls_array.values()) > 10:
+        plt.yticks(range(0, max(rolls_array.values()) + 1, 2))
     else:
-        plt.yticks(range(0, max(array.values()) + 1))
+        plt.yticks(range(0, max(rolls_array.values()) + 1))
 
-# ------------------------------ Dice Roll Logic ----------------------------- #
+if __name__ == '__main__':
+    
+    # ----------------------------------- Setup ---------------------------------- #
 
-while True:
-    print("-------------------------------------------")
-    # Roll dice
-    print("Rolling...")
-    time.sleep(1)
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    total = dice1 + dice2 
+    number_of_rolls = 1
+    setup()
+    dice_faces = custom_dice_setup()
+    roll_history = create_roll_history(dice_faces)
+    print(roll_history)
+    print("Let's get rolling!\n")
+    time.sleep(0.9)
 
-    print(f'Roll #{number_of_rolls}: {dice1}, {dice2}\n')
+    # -------------------------------- Execution --------------------------------- #
 
-    time.sleep(1)
+    while True:
+        print("-------------------------------------------")
 
-    # Update roll history
-    roll_history[total] += 1
+        # Roll dice
+        print("Rolling...")
+        time.sleep(1)
 
-    # Print roll frequency graph
-    create_graph(roll_history, number_of_rolls)
+        rolls = []
+
+        for dice in dice_faces:
+            rolls.append(random.randint(1, dice))
+
+        total = sum(rolls)
+
+        print(f'Roll #{number_of_rolls}: {str(rolls)[1:-1]}')
+
+        time.sleep(1)
+
+        # Update roll history
+        roll_history[total] += 1
+
+        # Print roll frequency graph
+        create_graph(roll_history, number_of_rolls, dice_faces)
+        plt.show()
+
+        # Ask for more rolls
+        while True:
+            try:
+                roll_again = input("\nRoll again? Y or N\n")
+                if roll_again.upper() not in ["Y", "N"]:
+                    raise TypeError
+            except TypeError:
+                print("Only Y or N")
+                continue
+            else:
+                break
+
+        if roll_again.upper() == "N":
+            break
+    
+        # Update roll number
+        number_of_rolls+= 1
+
+        continue
+
+    time.sleep(0.3)
+    print("\nThanks for rolling with us.")
+
+    create_graph(roll_history, number_of_rolls, dice_faces)
+    plt.title(f'Dice Rolls this game. Total Rolls = {number_of_rolls}')
     plt.show()
 
-    # Ask for more rolls
-    while True:
-        try:
-            roll_again = input("Roll again? Y or N\n")
-            if roll_again.upper() not in ["Y", "N"]:
-                raise TypeError
-        except TypeError:
-            print("Only Y or N")
-            continue
-        else:
-            break
-
-    if roll_again.upper() == "N":
-        break
-    
-    # Update roll number
-    number_of_rolls += 1
-
-    continue
-
-time.sleep(0.3)
-print("\nThanks for rolling with us.")
-
-create_graph(roll_history, number_of_rolls)
-plt.title(f'Dice Rolls this game. Total Rolls = {number_of_rolls}')
-plt.show()
 
