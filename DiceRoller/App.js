04d05815d57612690b04c501f8117ee55c9950ef
aiman.wahizam <aiman.wahizam@nology.io>
2024-04-25 20:35:38 +0400

    rough spacing of title and button

--- DiceRoller/App.js
+++ DiceRoller/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 
 export default function App() {
@@ -17,8 +17,14 @@ export default function App() {
 
   return (
     <View style={page.container}>
-      <Text style={title.title}>Dice Roller</Text>
-      <StatusBar style="auto" />
+      <View style={page.title_container}>
+        <Text style={title.title}>Dice Roller</Text>
+        <StatusBar style="auto" />
+      </View>
+      {/* <View style={page.empty}></View> */}
+      <View style={page.button_container}>
+        <Button title='Start'/>
+      </View>
     </View>
   );
 }
@@ -27,11 +33,28 @@ export default function App() {
 
 const page = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    paddingTop:150,
+    // marginBottom: 200
   },
+
+  title_container: {
+    flex:0.75,
+    borderColor: 'black',
+    borderWidth: 5,
+    fontFamily: 'Jersey15Charted-Regular',
+    fontSize: 70
+  },
+
+  button_container: {
+    flex:2,
+    borderWidth:5,
+    height:200,
+    justifyContent: 'center'
+  }
 });
 
 const title = StyleSheet.create({
